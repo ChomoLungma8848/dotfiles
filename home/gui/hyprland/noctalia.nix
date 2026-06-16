43cd28bd9b9fb37fe5463e5e9aa9c93ce0e34fd2
ChomoLungma8848 <chomo_lungma@icloud.com>
2026-06-16 16:56:42 +0900

    fix(hyprland): use absolute path for noctalia wallpaper

--- home/gui/hyprland/noctalia.nix
+++ home/gui/hyprland/noctalia.nix
@@ -11,7 +11,7 @@
   home.file.".cache/noctalia/wallpapers.json" = {
     text = builtins.toJSON{
       wallpapers = {
-        "DP-1" = "../../../wallpaper/wallpaper-1.png";
+        "DP-1" = "/home/chomo/ghq/github.com/ChomoLungma8848/dotfiles/wallpaper/wallpaper-1.png";
       };
     };
   };
