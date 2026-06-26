9eab76b59d2bba4c2ede65e8615defaa35a3294d
ChomoLungma8848 <chomo_lungma@icloud.com>
2026-06-26 09:06:32 +0900

    chore(starship): remove tmux session display module

--- home/programs/starship.nix
+++ home/programs/starship.nix
@@ -6,13 +6,6 @@
       add_newline = false;
       format = "$custom$directory$git_branch$git_status$nix_shell$character";
 
-      custom.tmux = {
-        command = "tmux display-message -p '#S'";
-        when = ''test -n "$TMUX"'';
-        format = "[\\[$output\\]]($style)";
-        style = "green";
-      };
-
       git_branch = {
         format = "[$symbol$branch]($style) ";
         symbol = "@";
