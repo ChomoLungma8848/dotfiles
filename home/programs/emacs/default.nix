23b2297b002f2af7b956538b1a7c6e520b8d4617
ChomoLungma8848 <chomo_lungma@icloud.com>
2026-07-06 11:44:34 +0900

    chore(emacs): remove nvfetcher sources and disable extra packages

--- home/programs/emacs/default.nix
+++ home/programs/emacs/default.nix
@@ -1,29 +1,29 @@
-{ pkgs, ... }:
-let
-  sources = pkgs.callPackage ../../_sources/generated.nix { };
-in
+# { pkgs, ... }:
+# let
+#   sources = pkgs.callPackage ../../_sources/generated.nix { };
+# in
 {
   programs.emacs = {
     enable = true;
-    extraPackages =
-      epkgs: with epkgs; [
-        leaf
-        leaf-convert
-        doom-themes
-        doom-modeline
-        org-roam
-        org-pomodoro
-        vertico
-        orderless
-        which-key
-        (epkgs.melpaBuild {
-          pname = "nskk";
-          version = "0.2.1";
-          src = sources.nskk.src;
-          files = ''("src/*.el")'';
-          ignoreCompilationError = false;
-        })
-      ];
-    extraConfig = builtins.readFile ./init.el;
+    # extraPackages =
+    #   epkgs: with epkgs; [
+    #     leaf
+    #     leaf-convert
+    #     doom-themes
+    #     doom-modeline
+    #     org-roam
+    #     org-pomodoro
+    #     vertico
+    #     orderless
+    #     which-key
+    #     (epkgs.melpaBuild {
+    #       pname = "nskk";
+    #       version = "0.2.1";
+    #       src = sources.nskk.src;
+    #       files = ''("src/*.el")'';
+    #       ignoreCompilationError = false;
+    #     })
+    #   ];
+    # extraConfig = builtins.readFile ./init.el;
   };
 }
