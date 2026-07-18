2ca75c7bbf90b4f9388f47f309a739884ed0714d
ChomoLungma8848 <chomo_lungma@icloud.com>
2026-07-19 00:00:31 +0900

    chore(home): allow insecure electron 40 for vesktop

--- home/gui.nix
+++ home/gui.nix
@@ -14,6 +14,12 @@
     ./gui/vesktop.nix
   ];
 
+  # vesktop が EOL の electron 40 に依存しているため一時的に許可。
+  # nixpkgs 側で vesktop が新しい electron に移行したら削除する。
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-40.10.5"
+  ];
+
   home = {
     packages = with pkgs; [
       google-chrome
